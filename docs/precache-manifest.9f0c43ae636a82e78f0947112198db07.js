4ad4548e4b4a2fbb95dac6c5945e0a50c8974842
Jeff Brausam <48197040+Jeff-Brausam@users.noreply.github.com>
2020-07-15 12:04:28 -0700

    Build to Docs update

---
+++ docs/precache-manifest.9f0c43ae636a82e78f0947112198db07.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9aeaff57cb3fde1f1e625d1cb5a9d83",
+    "url": "/mockup-online-store/index.html"
+  },
+  {
+    "revision": "68ab9a521ec4f2a8a205",
+    "url": "/mockup-online-store/static/css/3.7005a3c3.chunk.css"
+  },
+  {
+    "revision": "59e14a202d7ae64922da",
+    "url": "/mockup-online-store/static/css/4.99133fff.chunk.css"
+  },
+  {
+    "revision": "ea412cbae8cbde358ada",
+    "url": "/mockup-online-store/static/css/5.34186666.chunk.css"
+  },
+  {
+    "revision": "bdf3ceab41b3a05a9b72",
+    "url": "/mockup-online-store/static/css/main.76e0371f.chunk.css"
+  },
+  {
+    "revision": "ae8b697dd61c5495055e",
+    "url": "/mockup-online-store/static/js/2.c08d1f79.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mockup-online-store/static/js/2.c08d1f79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68ab9a521ec4f2a8a205",
+    "url": "/mockup-online-store/static/js/3.08bcf351.chunk.js"
+  },
+  {
+    "revision": "59e14a202d7ae64922da",
+    "url": "/mockup-online-store/static/js/4.50b3da1c.chunk.js"
+  },
+  {
+    "revision": "ea412cbae8cbde358ada",
+    "url": "/mockup-online-store/static/js/5.f8a1b83c.chunk.js"
+  },
+  {
+    "revision": "bdf3ceab41b3a05a9b72",
+    "url": "/mockup-online-store/static/js/main.b93642fa.chunk.js"
+  },
+  {
+    "revision": "77e4fcba05606603f544",
+    "url": "/mockup-online-store/static/js/runtime-main.a58e50df.js"
+  },
+  {
+    "revision": "95571b5e2b8e42c90b1a9d83b5c146b4",
+    "url": "/mockup-online-store/static/media/MockupBrand.95571b5e.svg"
+  }
+]);
